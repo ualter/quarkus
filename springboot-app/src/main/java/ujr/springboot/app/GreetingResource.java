b20683c320feb818d23b25d069191e3f306bccc4
Ualter Azambuja Junior <ualter.junior@gmail.com>
2019-07-23 10:47:03 +0200

    .

--- springboot-app/src/main/java/ujr/springboot/app/GreetingResource.java
+++ springboot-app/src/main/java/ujr/springboot/app/GreetingResource.java
@@ -1,5 +1,8 @@
 package ujr.springboot.app;
 
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -13,7 +16,13 @@ public class GreetingResource {
 
 	@GetMapping(path = "/greeting/{name}", produces = MediaType.TEXT_PLAIN_VALUE)
 	public String greeting(@PathVariable(value = "name") String name) {
-		return "Hello from SpringBoot, " + name;
+		String ip = "";
+		try {
+			ip = InetAddress.getLocalHost().getHostAddress();
+		} catch (UnknownHostException e) {
+			ip = "Ops... " + e.getMessage();
+		}
+		return "Hello from SpringBoot, " + name  + "\n\n*Server_IP --> " + ip;
 	}
 
 }
